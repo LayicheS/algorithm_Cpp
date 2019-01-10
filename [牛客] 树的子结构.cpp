d8ae745c0caed2057ef4b696418fa992fc4e65a8
xiefz <13269590312@163.com>
2019-01-11 00:01:30 +0800

    update

---
+++ [牛客] 树的子结构.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<map>
+#include<vector>
+#include<string>
+#include<regex>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2)
+{
+    if(pRoot2==NULL or pRoot1==NULL) return false;
+    if(pRoot1->val==pRoot2->val and ((pRoot2->left==NULL or HasSubtree(pRoot1->left,pRoot2->left))
+    and (pRoot2->right==NULL or HasSubtree(pRoot1->right,pRoot2->right))))
+            return true;
+    else
+        return (HasSubtree(pRoot1->left,pRoot2) or HasSubtree(pRoot1->right,pRoot2));
+}
+
+int main(){
+    return 0;
+}
