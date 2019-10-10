198e174d6c891c5df3b55691f51d02f055c9dfb2
xie xie <xiexie@XiedeMacBook-Pro.local>
2019-10-10 18:06:49 +0800

    tmp

--- main.cpp
+++ main.cpp
@@ -11,11 +11,28 @@
 
 using namespace std;
 
-int qu(vector<vector<int>> &op, int l,int r){
-    int rst=0;
-    for(auto each:op){
-        if(each[0]>=l and each[0]<=r){
-            rst+=each[1];
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> degree(numCourses,0);
+    vector<vector<int>> edge(numCourses,vector<int>());
+    for(int i=0;i<prerequisites.size();i++){
+        degree[prerequisites[i][0]]++;
+        edge[prerequisites[i][1]].push_back(prerequisites[i][0]);
+    }
+    queue<int> que;
+    for(int i=0;i<numCourses;i++){
+        if(degree[i]==0){
+            que.push(i);
+        }
+    }
+    vector<int> rst();
+    while(!que.empty()){
+        int tmp=que.top();
+        que.pop();
+        for(int j=0;j<edge[tmp].size();j++){
+            degree[edge[tmp][j]]--;
+            if(!degree[edge[tmp][j]]){
+                que.push(edge[tmp][j]);
+            }
         }
     }
     return rst;
@@ -23,37 +40,6 @@ int qu(vector<vector<int>> &op, int l,int r){
 
 int main() {
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        int m;
-        cin>>m;
-        vector<int> arr(m);
-        for(int j=0;j<m;j++){
-            cin>>arr[j];
-        }
-        int ma=INT32_MIN,fi=0;
-        map<int,int> mp;
-        for(int j=0;j<m;j++){
-            int les = 0;
-            for(auto it:mp){
-                if(it.first<arr[j]){
-                    les+=it.second;
-                }
-                else break;
-            }
-            if(mp.count(arr[j])){
-                fi+=les -(j-mp[arr[j]]-les);
-                mp[arr[j]]++;
-            }
-            else{
-                fi += les - (j-les);
-                mp[arr[j]] = 1;
-            }
-            if(fi>ma){
-                ma=fi;
-            }
-        }
-        cout<<ma<<" "<<fi<<endl;
-    }
+
     return 0;
 }
